38fb053aa2735004d109c5395966e4c5f509b2c6
James Hawkins <jhawki2005@gmail.com>
2023-02-28 18:20:21 +0000

    more

--- frontend-2/src/router/index.js
+++ frontend-2/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue';
+import RegisterView from '@/views/RegisterView.vue';
+import NotFound from '@/views/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +16,16 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'notFound',
+      component: NotFound
     }
   ]
 })
---
+++ frontend-2/src/views/RegisterView.vue
@@ -0,0 +1,68 @@
+<template>
+    <form onsubmit="return false">
+        <p v-if="loading">registering...</p>
+        <p v-if="error">Failed to register</p>
+
+        <div class="fields">
+            <input v-model="username" />
+            <input v-model="studentId" />
+            <input v-model="password" type="password" />
+            <input v-model="confirmPassword" type="password" />
+
+            <button @click="submit()">Register</button>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            loading: false,
+            error: false,
+
+            username: '',
+            password: '',
+            confirmPassowrd: '',
+            studentId: ''
+        }
+    },
+    methods: {
+        async submit() {
+            if (this.loading) return
+
+            this.loading = true
+            this.error = false
+
+            try {
+                const res = await fetch(`${window._env_.FRONTEND_API_URL}/api/v1/auth/register`, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        username: this.username.trim(),
+                        password: this.password.trim(),
+                        studentId: this.studentId.trim()
+                    }),
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                const data = await res.json()
+                if (!data.status) {
+                    throw new Error('failed to register')
+                }
+
+                this.$router.push('/')
+
+                this.loading = false
+                this.error = false
+            } catch (err) {
+                console.error('failed to register', err)
+                this.loading = false
+                this.error = true
+                return;
+            }
+        }
+    }
+}
+</script>
